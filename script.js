7ba484f5322429167e7655d4509b3745a070d6a8
wishba <wisnubayu34@gmail.com>
2026-04-01 23:39:27 +0700

    enter button

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+let keyCode = []
+
+document.getElementById('keypad-enter').addEventListener('click', () => {
+  console.log('ENTER: ' + keyCode)
+  keyCode = []
+})
+
 document.querySelectorAll('#keypad-number button').forEach((btn) => {
   btn.addEventListener('click', () => {
-    console.log(btn.textContent);
+    keyCode.push(btn.textContent.trim())
+    console.log(keyCode)
   })
 })
