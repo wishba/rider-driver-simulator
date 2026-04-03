7886baf06ce7f4146cf4bea08ce88f3b1ca98622
wishba <wisnubayu34@gmail.com>
2026-04-03 20:52:44 +0700

    hover pointer on every button

--- app/faiz-phone/page.tsx
+++ app/faiz-phone/page.tsx
@@ -18,77 +18,82 @@ export default function FaizPhonePage() {
         <div>
             <p className='overflow-x-hidden'>{display}_</p>
 
-            <button onClick={handleEnter}>ENTER</button>
+            <button
+                className='cursor-pointer'
+                onClick={handleEnter}
+            >
+                ENTER
+            </button>
 
             <div className='grid grid-cols-3'>
                 <button
-                    className='border'
+                    className='border cursor-pointer'
                     onClick={handleKeypad}
                 >
                     1
                 </button>
                 <button
-                    className='border'
+                    className='border cursor-pointer'
                     onClick={handleKeypad}
                 >
                     2
                 </button>
                 <button
-                    className='border'
+                    className='border cursor-pointer'
                     onClick={handleKeypad}
                 >
                     3
                 </button>
                 <button
-                    className='border'
+                    className='border cursor-pointer'
                     onClick={handleKeypad}
                 >
                     4
                 </button>
                 <button
-                    className='border'
+                    className='border cursor-pointer'
                     onClick={handleKeypad}
                 >
                     5
                 </button>
                 <button
-                    className='border'
+                    className='border cursor-pointer'
                     onClick={handleKeypad}
                 >
                     6
                 </button>
                 <button
-                    className='border'
+                    className='border cursor-pointer'
                     onClick={handleKeypad}
                 >
                     7
                 </button>
                 <button
-                    className='border'
+                    className='border cursor-pointer'
                     onClick={handleKeypad}
                 >
                     8
                 </button>
                 <button
-                    className='border'
+                    className='border cursor-pointer'
                     onClick={handleKeypad}
                 >
                     9
                 </button>
                 <button
-                    className='border'
+                    className='border cursor-pointer'
                     onClick={handleKeypad}
                 >
                     *
                 </button>
                 <button
-                    className='border'
+                    className='border cursor-pointer'
                     onClick={handleKeypad}
                 >
                     0
                 </button>
                 <button
-                    className='border'
+                    className='border cursor-pointer'
                     onClick={handleKeypad}
                 >
                     #
